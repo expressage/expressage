e8f56cdcc4df64121d1d7124c53ae0e2cee6502c
zk1111 <1813504008@qq.com>
2018-12-28 09:51:14 +0800

    添加基本类

--- src/main/java/com/expressage/mapper/EmployeeMapper.java
+++ src/main/java/com/expressage/mapper/EmployeeMapper.java
@@ -1,17 +1,23 @@
 package com.expressage.mapper;
 
-import com.expressage.pojo.Employee;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
+import com.expressage.pojo.Employee;
+@Mapper
 public interface EmployeeMapper {
     int deleteByPrimaryKey(Integer eid);
 
     int insert(Employee record);
 
     int insertSelective(Employee record);
-
+    
     Employee selectByPrimaryKey(Integer eid);
 
     int updateByPrimaryKeySelective(Employee record);
 
     int updateByPrimaryKey(Employee record);
+    
+    @Select("select * from employee where username = #{username}")
+    Employee selByUsername(String username);
 }
--- src/main/java/com/expressage/mapper/PowerMapper.java
+++ src/main/java/com/expressage/mapper/PowerMapper.java
@@ -1,9 +1,21 @@
 package com.expressage.mapper;
 
-import com.expressage.pojo.Power;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.expressage.pojo.Power;
+@Mapper
 public interface PowerMapper {
     int insert(Power record);
 
     int insertSelective(Power record);
+    
+    @Select("SELECT * FROM power")
+	List<Power> selAll();
+	
+	@Select("SELECT * FROM `power` p LEFT JOIN role_power  rp ON p.pid = rp.pid LEFT JOIN employee_role er ON er.rid =rp.rid WHERE er.eid=#{eid} GROUP BY p.pid")
+	List<Power> selByUserId(Map<String,Object> map);
 }
